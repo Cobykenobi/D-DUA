b75e3cae2ee9ef2c0e6d9fc60a515f9d3b5ac31d
Cobykenobi <corby666@gmail.com>
2025-06-10 14:11:55 +0300

    Revert "Fix admin token usage"

--- frontend/src/store/user.js
+++ frontend/src/store/user.js
@@ -6,19 +6,18 @@ export const useUserStore = create(
   persist(
     (set) => ({
       user: null,
-      token: null,
       setUser: (user, token) => {
         localStorage.setItem("token", token);
-        set({ user, token });
+        set({ user });
       },
       logout: () => {
         localStorage.removeItem("token");
-        set({ user: null, token: null });
+        set({ user: null });
       },
     }),
     {
       name: "user-storage",
-      partialize: (state) => ({ user: state.user, token: state.token }),
+      partialize: (state) => ({ user: state.user }),
     }
   )
 );
