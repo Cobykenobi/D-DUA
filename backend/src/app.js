7de6a5e4ba0862b04d0120222bfe9647f3c23c7c
Cobykenobi <corby666@gmail.com>
2025-06-10 13:34:40 +0300

    Import all route modules

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,16 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/auth"));
+app.use("/api/character", require("./routes/character"));
+app.use("/api/characteristic", require("./routes/characteristic"));
+app.use("/api/inventory", require("./routes/inventory"));
+app.use("/api/map", require("./routes/map"));
+app.use("/api/music", require("./routes/music"));
+app.use("/api/profession", require("./routes/profession"));
+app.use("/api/race", require("./routes/race"));
+app.use("/api/roll", require("./routes/roll"));
+app.use("/api/session", require("./routes/session"));
+app.use("/api/user", require("./routes/user"));
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
