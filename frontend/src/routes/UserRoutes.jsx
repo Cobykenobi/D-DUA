6546a873a51449e6f3d69a04cd7c13cf1b6cad36
Cobykenobi <corby666@gmail.com>
2025-06-10 13:24:37 +0300

    Remove duplicate character creation page

--- frontend/src/routes/UserRoutes.jsx
+++ frontend/src/routes/UserRoutes.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import CharactersPage from '../pages/CharactersPage';
-import CharacterCreatePage from '../pages/CharacterCreatePage';
+import CreateCharacterPage from '../pages/CreateCharacterPage';
 import CharacterEditPage from '../pages/CharacterEditPage';
 
 export default function UserRoutes() {
   return (
     <Routes>
       <Route path="characters" element={<CharactersPage />} />
-      <Route path="characters/new" element={<CharacterCreatePage />} />
+      <Route path="characters/new" element={<CreateCharacterPage />} />
       <Route path="characters/:id/edit" element={<CharacterEditPage />} />
     </Routes>
   );
