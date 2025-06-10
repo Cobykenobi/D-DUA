9f222ac28bc842149b559f57f96d8e3a2a312cb9
Cobykenobi <corby666@gmail.com>
2025-06-10 22:08:37 +0300

    Remove placeholder pages and update routes

--- frontend/src/routes/UserRoutes.jsx
+++ frontend/src/routes/UserRoutes.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import CharactersPage from '../pages/CharactersPage';
+import CharacterListPage from '../pages/CharacterListPage';
 import CharacterCreatePage from '../pages/CharacterCreatePage';
 import CharacterEditPage from '../pages/CharacterEditPage';
 
 export default function UserRoutes() {
   return (
     <Routes>
-      <Route path="characters" element={<CharactersPage />} />
+      <Route path="characters" element={<CharacterListPage />} />
       <Route path="characters/new" element={<CharacterCreatePage />} />
       <Route path="characters/:id/edit" element={<CharacterEditPage />} />
     </Routes>
