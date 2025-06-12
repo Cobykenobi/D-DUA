280692a2fddaac3cde7e34d31fe1a361056ac501
Cobykenobi <corby666@gmail.com>
2025-06-12 13:26:25 +0300

    fix: add missing context and router imports

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,9 @@
 import "./i18n";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { SettingsProvider } from './context/SettingsContext';
+import { ToastProvider } from './context/ToastContext';
 import App from './App';
 import './index.css';
 
