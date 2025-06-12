e08443c116fc7ce519f3fead3cf7a72d382c0d32
Cobykenobi <corby666@gmail.com>
2025-06-12 17:33:37 +0300

    Add files via upload

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,22 +1,18 @@
-import "./i18n";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 import { SettingsProvider } from './context/SettingsContext';
 import { ToastProvider } from './context/ToastContext';
-import AxiosToastProvider from './components/AxiosToastProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <SettingsProvider>
-        <ToastProvider>
-          <AxiosToastProvider />
+    <SettingsProvider>
+      <ToastProvider>
+        <BrowserRouter>
           <App />
-        </ToastProvider>
-      </SettingsProvider>
-    </BrowserRouter>
+        </BrowserRouter>
+      </ToastProvider>
+    </SettingsProvider>
   </React.StrictMode>
 );
