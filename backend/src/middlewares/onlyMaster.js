55ac25bc29fdae2061a0059ce8a3883f00f0b095
Cobykenobi <corby666@gmail.com>
2025-06-14 17:29:31 +0300

    Update onlyMaster.js

--- backend/src/middlewares/onlyMaster.js
+++ backend/src/middlewares/onlyMaster.js
@@ -1,4 +1,17 @@
-module.exports = (req, res, next) => {
-  if (req.user && (req.user.role === 'master' || req.user.role === 'admin')) return next();
-  return res.status(403).json({ message: 'Forbidden' });
-};
+diff --git a/backend/src/middleware/onlyMaster.js b/backend/src/middleware/onlyMaster.js
+index aee516300e9156fbff061b8f9c90c5b0977afeb9..a6226083c576c919fd86836acdda16e86fdce109 100644
+--- a/backend/src/middleware/onlyMaster.js
++++ b/backend/src/middleware/onlyMaster.js
+@@ -1,4 +1,8 @@
+-module.exports = (req, res, next) => {
+-  if (req.user && (req.user.role === 'master' || req.user.role === 'admin')) return next();
+-  return res.status(403).json({ message: 'Forbidden' });
+-};
++function onlyMaster(req, res, next) {
++  if (req.user && (req.user.role === 'master' || req.user.role === 'admin')) {
++    return next();
++  }
++  return res.status(403).json({ message: 'Forbidden' });
++}
++
++module.exports = onlyMaster;
