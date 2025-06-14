1b3d6b4339d0ba780ea1b87239dddc1229d2febf
Cobykenobi <corby666@gmail.com>
2025-06-15 00:26:17 +0300

    Add files via upload

---
+++ backend/src/middleware/onlyMaster.js
@@ -0,0 +1,8 @@
+function onlyMaster(req, res, next) {
+  if (req.user && (req.user.role === 'master' || req.user.role === 'admin')) {
+    return next();
+  }
+  return res.status(403).json({ message: 'Forbidden' });
+}
+
+module.exports = onlyMaster;
