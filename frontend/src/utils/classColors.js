4c779b1a103306484037e5f9c8c19c288a38745d
Cobykenobi <corby666@gmail.com>
2025-06-21 12:40:36 +0300

    Add animated player card with modal details

---
+++ frontend/src/utils/classColors.js
@@ -0,0 +1,14 @@
+export function getClassBorderColor(profession) {
+  const map = {
+    warrior: 'border-red-600',
+    mage: 'border-blue-600',
+    archer: 'border-green-600',
+    paladin: 'border-yellow-600',
+    bard: 'border-purple-600',
+    healer: 'border-teal-600',
+    druid: 'border-green-700',
+  };
+  if (!profession) return 'border-dndgold';
+  const key = profession.toLowerCase();
+  return map[key] || 'border-dndgold';
+}
