cec5196bcf4b0b9186b99558d253fc604c1317c0
Cobykenobi <corby666@gmail.com>
2025-06-22 16:52:46 +0300

    Update login to store token and translations

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,10 +1,13 @@
-// LoginPage.jsx (patched)
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import axios from 'axios';
+import { useUserStore } from '../store/user';
 
 function LoginPage() {
   const navigate = useNavigate();
+  const { t } = useTranslation();
+  const setUser = useUserStore((s) => s.setUser);
 
   const handleLogin = async (event) => {
     event.preventDefault();
@@ -15,19 +18,16 @@ function LoginPage() {
       password: password.value,
     });
 
-    const user = res.data;
-    if (user.role === 'gm') {
-      navigate('/gm-dashboard');
-    } else {
-      navigate('/characters');
-    }
+    const { user, token } = res.data;
+    setUser(user, token);
+    navigate(user.role === 'gm' ? '/gm-dashboard' : '/characters');
   };
 
   return (
     <form onSubmit={handleLogin}>
-      <input name='username' placeholder='Username' />
-      <input name='password' placeholder='Password' type='password' />
-      <button type='submit'>Login</button>
+      <input name='username' placeholder={t('login')} />
+      <input name='password' placeholder={t('password')} type='password' />
+      <button type='submit'>{t('login')}</button>
     </form>
   );
 }
