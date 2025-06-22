b4dbee1d8c9c198f2398ffecc106962abdeb9809
Cobykenobi <corby666@gmail.com>
2025-06-22 19:59:47 +0300

    Configure Vite build chunks

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,22 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1024,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('i18next') || id.includes('react-i18next')) {
+              return 'i18n';
+            }
+            if (id.includes('react') || id.includes('react-dom')) {
+              return 'react';
+            }
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 });
