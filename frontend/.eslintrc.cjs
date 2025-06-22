14c6560661a94b8fe872a10b79e95c19d956b31f
Cobykenobi <corby666@gmail.com>
2025-06-23 02:10:35 +0300

    Add ESLint setup for frontend

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
