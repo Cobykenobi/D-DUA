ee3262ebd725766e99cf063c75a6a6fe397fbc72
Cobykenobi <corby666@gmail.com>
2025-06-22 20:01:04 +0300

    feat(frontend): add lazy-loaded pages

--- frontend/src/routes/UserRoutes.jsx
+++ frontend/src/routes/UserRoutes.jsx
@@ -1,13 +1,19 @@
-import CharacterListPage from '../pages/CharacterListPage';
-import CharacterCreatePage from '../pages/CharacterCreatePage';
-import CharacterEditPage from '../pages/CharacterEditPage';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Loader from '../components/Loader';
+
+const CharacterListPage = React.lazy(() => import('../pages/CharacterListPage'));
+const CharacterCreatePage = React.lazy(() => import('../pages/CharacterCreatePage'));
+const CharacterEditPage = React.lazy(() => import('../pages/CharacterEditPage'));
 
 export default function UserRoutes() {
   return (
-    <Routes>
-      <Route path="characters" element={<CharacterListPage />} />
-      <Route path="characters/new" element={<CharacterCreatePage />} />
-      <Route path="characters/:id/edit" element={<CharacterEditPage />} />
-    </Routes>
+    <React.Suspense fallback={<Loader />}>
+      <Routes>
+        <Route path="characters" element={<CharacterListPage />} />
+        <Route path="characters/new" element={<CharacterCreatePage />} />
+        <Route path="characters/:id/edit" element={<CharacterEditPage />} />
+      </Routes>
+    </React.Suspense>
   );
 }
