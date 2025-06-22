6a333efd0ad02e6d3c4ab720d72ab014776da44f
Cobykenobi <corby666@gmail.com>
2025-06-22 20:50:20 +0300

    Remove unused getPostLoginRedirect util and related test

--- frontend/tests/loginRedirect.test.cjs
+++
@@ -1,8 +0,0 @@
-/** @jest-environment node */
-const getPostLoginRedirect = require('../src/utils/getPostLoginRedirect.cjs');
-
-test('gm role redirects to gm dashboard', () => {
-  expect(getPostLoginRedirect('gm')).toBe('/gm-dashboard');
-  expect(getPostLoginRedirect('admin')).toBe('/gm-dashboard');
-  expect(getPostLoginRedirect('player')).toBe('/characters');
-});
