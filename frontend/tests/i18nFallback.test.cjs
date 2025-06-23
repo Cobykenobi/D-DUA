c9b507fc24c00057acd549faff8bdc191d7cd2a3
Cobykenobi <corby666@gmail.com>
2025-06-23 23:47:43 +0300

    Add unknown translation key and tests

---
+++ frontend/tests/i18nFallback.test.cjs
@@ -0,0 +1,22 @@
+/** @jest-environment node */
+
+let en, ua, translateOrRaw;
+beforeAll(async () => {
+  ({ default: translateOrRaw } = await import('../src/utils/translateOrRaw.js'));
+  ({ default: en } = await import('../src/locales/en.json', { assert: { type: 'json' } }));
+  ({ default: ua } = await import('../src/locales/ua.json', { assert: { type: 'json' } }));
+});
+
+function makeT(data) {
+  return (key) => key.split('.').reduce((o, p) => (o || {})[p], data) ?? key;
+}
+
+test('english missing key uses unknown translation', () => {
+  const t = makeT(en);
+  expect(translateOrRaw(t, 'stats.missing')).toBe(en.unknown);
+});
+
+test('ukrainian missing key uses unknown translation', () => {
+  const t = makeT(ua);
+  expect(translateOrRaw(t, 'stats.missing')).toBe(ua.unknown);
+});
