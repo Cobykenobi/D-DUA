50e7cd37a3d3f9c160faa8752d92aab265e4c74a
Cobykenobi <corby666@gmail.com>
2025-06-23 21:46:02 +0300

    Use final gender once

--- backend/src/controllers/characterController.js
+++ backend/src/controllers/characterController.js
@@ -119,7 +119,6 @@ exports.create = async (req, res) => {
   const newChar = new Character({
       user: req.user.id,
       name,
-      gender,
       description,
       image: avatar,
       gender: finalGender,
