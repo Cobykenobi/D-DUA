e3efb3984c0eb461bbe9c62b56cf41cc27a752c8
Cobykenobi <corby666@gmail.com>
2025-06-26 15:33:37 +0300

    Remove unused settings route

--- backend/src/routes/settings.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { saveSettings } = require("../controllers/settingsController");
-const authMiddleware = require("../middlewares/authMiddleware");
-
-router.post("/", authMiddleware, saveSettings);
-
-module.exports = router;
